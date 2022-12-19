60033db6a014407aa005b9461800d01b877f7e2b
batyril <237x237@gmail.ru>
2022-12-19 16:28:46 +0300

    added basic components

---
+++ src/components/app/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AppHeader from '../appHeader/AppHeader';
+import RandomChar from '../randomChar/RandomChar';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+import decoration from '../../resources/img/vision.png';
+
+function App() {
+  return (
+    <div className='app'>
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className='char__content'>
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className='bg-decoration' src={decoration} alt='vision' />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AppHeader.scss';
+
+function AppHeader() {
+  return (
+    <header className='app__header'>
+      <h1 className='app__title'>
+        <a href='#'>
+          <span>Marvel</span> information portal
+        </a>
+      </h1>
+      <nav className='app__menu'>
+        <ul>
+          <li>
+            <a href='#'>Characters</a>
+          </li>
+          /
+          <li>
+            <a href='#'>Comics</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default AppHeader;
